d9216c6034ad0bb0389dc1bfb83f6d4362731bf2
Matthew Slade <matthew@gyft.com>
2016-05-03 11:30:06 -0700

    added search

--- app/src/main/java/com/sladematthew/apm/MainActivity.kt
+++ app/src/main/java/com/sladematthew/apm/MainActivity.kt
@@ -4,13 +4,29 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
+import android.text.Editable
+import android.text.TextWatcher
 import com.sladematthew.apm.model.Password
 import com.sladematthew.apm.model.PasswordList
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
-class MainActivity: APMActivity(),PasswordAdapter.OnItemClickListener
+class MainActivity: APMActivity(),PasswordAdapter.OnItemClickListener, TextWatcher
 {
+    var adapter:PasswordAdapter?=null
+
+    override fun afterTextChanged(s: Editable?) {
+        adapter?.filter?.filter(s)
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+    }
+
     override fun onLongClick(viewHolder: PasswordAdapter.ViewHolder, item: Password, position: Int) {
         var intent = Intent(this, EditPasswordActivity::class.java)
         intent.putExtra(Constants.IntentKey.PASSWORD,item)
@@ -30,6 +46,7 @@ class MainActivity: APMActivity(),PasswordAdapter.OnItemClickListener
         recyclerView.itemAnimator = DefaultItemAnimator()
         recyclerView.setHasFixedSize(true)
         recyclerView.addItemDecoration(DividerDecoration(this))
+        searchBox.addTextChangedListener(this)
         addPassword.setOnClickListener{startActivity(Intent(this, EditPasswordActivity::class.java))}
         loadPasswordList()
     }
@@ -38,11 +55,11 @@ class MainActivity: APMActivity(),PasswordAdapter.OnItemClickListener
     {
         fun callback()
         {
-            var adapter = PasswordAdapter(ArrayList(authenticationManager!!.passwordList!!.passwords.values),this)
+            adapter = PasswordAdapter(ArrayList(authenticationManager!!.passwordList!!.passwords.values),this)
             recyclerView.adapter = adapter;
-            adapter.onItemClickListener = this;
+            adapter?.onItemClickListener = this;
         }
-        authenticationManager!!.loadPasswordList(::callback)
+        authenticationManager?.loadPasswordList(::callback)
     }
 
     override fun onResume() {
--- app/src/main/java/com/sladematthew/apm/PasswordAdapter.kt
+++ app/src/main/java/com/sladematthew/apm/PasswordAdapter.kt
@@ -1,27 +1,78 @@
 package com.sladematthew.apm
 
 import android.support.v7.widget.RecyclerView
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
+import android.widget.Filter
+import android.widget.Filterable
 import com.sladematthew.apm.model.Password
 
 import com.sladematthew.apm.model.PasswordList
 import kotlinx.android.synthetic.main.item_password.view.*
+import java.util.*
+import java.util.regex.Pattern
 
-class PasswordAdapter(var passwords: List<Password>,var onItemClickListener: OnItemClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class PasswordAdapter(var passwords: List<Password>,var onItemClickListener: OnItemClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>(), Filterable {
+
+    var filtered:List<Password>?=null
+
+
+    override fun getFilter(): Filter? {
+        return object :Filter()
+        {
+            override fun publishResults(constraint: CharSequence?, results: Filter.FilterResults?) {
+                setAndSortData(results!!.values as ArrayList<Password>)
+                notifyDataSetChanged()
+            }
+
+            override fun performFiltering(constraint: CharSequence?): Filter.FilterResults? {
+                val filterResults = Filter.FilterResults()
+                val tmp = ArrayList<Password>()
+                if (constraint == null || "" == constraint)
+                {
+                    tmp.addAll(passwords)
+                }
+                else
+                {
+                    for (item in passwords)
+                    {
+                        if (matchString(item.label, constraint))
+                            tmp.add(item)
+                    }
+                }
+                filterResults.values = tmp
+                filterResults.count = tmp.size
+                return filterResults
+            }
+
+        }
+    }
+
+    init
+    {
+        setAndSortData(passwords)
+    }
+
+    fun setAndSortData(list:List<Password>)
+    {
+        filtered = list
+        Collections.sort(filtered,{ lhs, rhs -> lhs.label.compareTo(rhs.label)});
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder? {
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_password, parent, false))
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as ViewHolder).itemView.name.text = passwords[position].label
+        (holder as ViewHolder).itemView.name.text = filtered!![position].label
     }
 
     override fun getItemCount(): Int {
-        return passwords.size;
+        return filtered!!.size;
     }
 
     interface OnItemClickListener {
@@ -43,7 +94,7 @@ class PasswordAdapter(var passwords: List<Password>,var onItemClickListener: OnI
             if (pos < 0) {
                 pos = 0
             }
-            onItemClickListener.onLongClick(this,passwords[pos]!!, pos)
+            onItemClickListener.onLongClick(this,filtered!![pos], pos)
             return true
         }
 
@@ -53,7 +104,19 @@ class PasswordAdapter(var passwords: List<Password>,var onItemClickListener: OnI
             if (pos < 0) {
                 pos = 0
             }
-            onItemClickListener.onClick(this,passwords[pos]!!, pos)
+            onItemClickListener.onClick(this,filtered!![pos], pos)
         }
+    }
+
+    fun matchString(value:String,constraint: CharSequence?):Boolean{
+        if (TextUtils.isEmpty(value)) {
+            return false
+        }
+        var s = constraint.toString()
+        if (TextUtils.isEmpty(s)) {
+            return false
+        }
+        s = Pattern.quote(s)
+        return Pattern.compile("$s.*|.*[-\\s/]$s.*", Pattern.CASE_INSENSITIVE).matcher(value).matches()
     }
 }
